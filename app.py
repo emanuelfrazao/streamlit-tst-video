0b87bb988fc278b9d42c6bbb745af2f5f99468af
emanuelfrazao <emanuel.j.frazao@gmail.com>
2023-06-13 16:59:06 +0100

    adding first model

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
+import av
 import cv2
 import streamlit as st
-st.write(cv2.CAP_V4L2)
-st.write(cv2.CAP_DSHOW)
-st.write(cv2.CAP_MSMF)
-st.title("Webcam Live Feed")
-run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(cv2.CAP_V4L2)
 
-while run:
-    _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(frame)
-else:
-    st.write('Stopped')
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+
+
+def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
+    image = frame.to_ndarray(format="bgr24")
+    cv2.putText(
+            image,
+            str(image.mean()),
+            (50,50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (255, 0, 0),
+            2,
+        )
+    return av.VideoFrame.from_ndarray(image, format="bgr24")
+
+threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
+threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
+
+
+def callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    img = cv2.cvtColor(cv2.Canny(img, threshold1, threshold2), cv2.COLOR_GRAY2BGR)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+webrtc_streamer(
+    key="object-detection",
+    mode=WebRtcMode.SENDRECV,
+    video_frame_callback=callback,
+    rtc_configuration={  # Add this config
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    },
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=True,
+)
